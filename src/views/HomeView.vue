7fdaf80b3f9007cf1f167db45121d88ab164c0f0
mihned01 <mihned01@easv365.dk>
2025-10-06 00:13:28 +0200

    feat(style): change Register button to Login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -124,7 +124,7 @@ const navigateToLogin = () => {
 
  .hero {
      width: 100%;
-     height: 60vh;
+     height: 70vh;
      display: flex;
      align-items: center;
      justify-content: space-between;
