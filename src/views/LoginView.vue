50cbf4a3d8f58ebae92ca75d444d78af1e109b19
mihned01 <mihned01@easv365.dk>
2025-10-27 14:09:00 +0100

    feat(style): restyling things

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,47 +1,61 @@
 <template>
-    <div class="auth-container">
-      <div class="form-container" :class="{ 'slide-left': isRegistering }">
-        
-        <!-- Login Form -->
-        <div class="form-section login">
-          <div class="form-content">
-            <div class="green-section login">
+  <div class="auth-container">
+    <div class="form-container" :class="{ 'slide-left': isRegistering }">
+
+      <!-- Login Form -->
+      <div class="form-section login">
+        <div class="form-content">
+          <div class="green-section login">
+
+            <button @click="goToHome" class="back-to-home-btn">
+              Back to Home
+            </button>
+
+
+            <div class="green-content">
               <h2>Hello, Welcome!</h2>
               <p>Don't have an account?</p>
               <button @click="toggleForm" class="switch-btn">Register</button>
             </div>
-            <div class="form-fields">
-              <h1>Login</h1>
-              <form @submit.prevent="loginUser">
-                <div class="input-group">
-                  <input type="email" v-model="email" placeholder="Username" required>
-                </div>
-                <div class="input-group">
-                  <input type="password" v-model="password" placeholder="Password" required>
-                </div>
-                <p class="forgot-password">Forgot password?</p>
-                <button type="submit" :disabled="loading" class="submit-btn">Login</button>
-              </form>
-            </div>
+          </div>
+          <div class="form-fields">
+            <h1>Login</h1>
+            <form @submit.prevent="loginUser">
+              <div class="input-group">
+                <input type="email" v-model="email" placeholder="Username" required>
+              </div>
+              <div class="input-group">
+                <input type="password" v-model="password" placeholder="Password" required>
+              </div>
+              <p class="forgot-password">Forgot password?</p>
+              <button type="submit" :disabled="loading" class="submit-btn">Login</button>
+            </form>
           </div>
         </div>
-  
-        <!-- Register Form -->
-        <div class="form-section register">
-          <div class="form-content">
-            <div class="form-fields">
-              <h1>Registration</h1>
-              <form @submit.prevent="registerUser">
-                <div class="input-group">
-                  <input type="email" v-model="regEmail" placeholder="Username" required>
-                </div>
-                <div class="input-group">
-                  <input type="password" v-model="regPassword" placeholder="Password" required>
-                </div>
-                <button type="submit" :disabled="loading" class="submit-btn">Register</button>
-              </form>
-            </div>
-            <div class="green-section register">
+      </div>
+
+      <!-- Register Form -->
+      <div class="form-section register">
+        <div class="form-content">
+          <div class="form-fields">
+            <h1>Registration</h1>
+            <form @submit.prevent="registerUser">
+              <div class="input-group">
+                <input type="email" v-model="regEmail" placeholder="Username" required>
+              </div>
+              <div class="input-group">
+                <input type="password" v-model="regPassword" placeholder="Password" required>
+              </div>
+              <button type="submit" :disabled="loading" class="submit-btn">Register</button>
+            </form>
+          </div>
+
+          <div class="green-section register">
+            <button @click="goToHome" class="back-to-home-btn-register">
+              Back to Home
+            </button>
+
+            <div class="green-content">
               <h2>Welcome back!</h2>
               <p>Already have an account?</p>
               <button @click="toggleForm" class="switch-btn">Login</button>
@@ -49,205 +63,284 @@
           </div>
         </div>
       </div>
-  
-      <div class="error" v-if="authError">
-        <p>{{ authError }}</p>
-      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { useAuth } from '../modules/useAuth.js'
-  import { useRouter } from 'vue-router'
-  
-  const router = useRouter()
-  const { login, register, authError, loading, isLoggedIn, currentUser } = useAuth()
-  const isRegistering = ref(false)
-  
-  const email = ref('')
-  const password = ref('')
-  const regEmail = ref('')
-  const regPassword = ref('')
-  
-  const toggleForm = () => {
-    isRegistering.value = !isRegistering.value
-  }
-  
-  const loginUser = async () => {
-    await login(email.value, password.value)
-    if (isLoggedIn.value) {
-      router.push('/dashboard')
-    }
-  }
-  
-  const registerUser = async () => {
-    await register(regEmail.value, regPassword.value)
-    if (!authError.value) {
-      isRegistering.value = false
-    }
-  }
-  </script>
-  
-  <style scoped>
-  .auth-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background-color: var(--color-main);
-    padding: 20px;
-  }
-  
-  .form-container {
-    width: 900px;
-    height: 500px;
-    position: relative;
-    overflow: hidden;
-    border-radius: 20px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-    transition: all 0.3s ease;
-  }
-  
-  .form-section {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    transition: all 0.6s ease;
-  }
-  
-  .form-content {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    background: white;
-  }
-  
-  .green-section {
-    width: 50%;
-    background-color: var(--color-main);
-    padding: 40px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: var(--color-text);
-  }
 
-  .green-section h2 {
-    font-size: 28px;
-    font-family: Poppins, sans-serif;
-    font-weight: 600;
-    margin-bottom: 10px;
-  }
+    <div class="error" v-if="authError">
+      <p>{{ authError }}</p>
+    </div>
+  </div>
+</template>
 
-  .green-section.login {
-    border-radius: 0 120px 120px 0;
-  }
+<script setup>
+import { ref } from 'vue'
+import { useAuth } from '../modules/useAuth.js'
+import { useRouter } from 'vue-router'
 
-  .green-section.register {
-    border-radius: 120px 0 0 120px;
-  }
-  
-  .form-fields {
-    width: 50%;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .input-group {
-    margin-bottom: 20px;
-  }
-  
-  input {
-    width: 100%;
-    padding: 12px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    font-size: 16px;
-  }
-  
-  .submit-btn {
-    width: 100%;
-    padding: 10px 20px;
-    margin-top: 10px;
-    background-color: var(--color-main);
-    color: var(--color-button);
-    border: 1.5px solid transparent;
-    border-radius: 8px;
-    font-size: 14px;
-    cursor: pointer;
-    transition: all 0.3s ease;
-    align-items: center;
-  }
+const router = useRouter()
+const { login, register, authError, loading, isLoggedIn, currentUser } = useAuth()
+const isRegistering = ref(false)
 
-  .submit-btn:hover {
-    background-color: var(--color-background);
-    color: var(--color-text);
-    border: 1.5px solid var(--color-main);
-  }
-  
-  .switch-btn {
-     padding: 10px 20px;
-     margin-top: 30px;
-     background-color: var(--color-button);
-     color: var(--color-background);
-     border: 1.5px solid transparent;
-     border-radius: 8px;
-     font-size: 14px;
-     cursor: pointer;
-     transition: all 0.3s ease;
-     align-items: center;
-  }
+const email = ref('')
+const password = ref('')
+const regEmail = ref('')
+const regPassword = ref('')
 
-  .switch-btn:hover {
-    background-color: var(--color-background);
-    color: var(--color-button);
-    border: 1.5px solid var(--color-button);
+const toggleForm = () => {
+  isRegistering.value = !isRegistering.value
+}
+
+const loginUser = async () => {
+  await login(email.value, password.value)
+  if (isLoggedIn.value) {
+    router.push('/dashboard')
   }
-  
-  .forgot-password {
-    font-size: 14px;
-    display: block;
-    text-align: center;
-    color: #666;
-    text-decoration: none;
-    margin: 10px 0;
+}
+
+const registerUser = async () => {
+  await register(regEmail.value, regPassword.value)
+  if (!authError.value) {
+    isRegistering.value = false
   }
-  
-  .error {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    background-color: #ff4444;
-    color: white;
-    padding: 10px 20px;
-    border-radius: 8px;
-  }
-  
-  /* Animation classes */
-  .slide-left .login {
-    transform: translateX(-100%);
-  }
-  
-  .register {
-    transform: translateX(100%);
-  }
-  
-  .slide-left .register {
-    transform: translateX(0);
-  }
-  
-  h1 {
-    margin-bottom: 30px;
-    color: var(--color-text);
-  }
-  
-  h2 {
-    margin-bottom: 20px;
-    color: var(--color-text);
-  }
-  </style>
+}
+
+const goToHome = () => {
+  router.push('/')
+}
+
+</script>
+
+<style scoped>
+.auth-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: var(--color-main);
+  padding: 20px;
+}
+
+.form-container {
+  width: 900px;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  transition: all 0.3s ease;
+}
+
+.form-section {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  transition: all 0.6s ease;
+}
+
+.form-content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: white;
+}
+
+.green-section {
+  width: 50%;
+  background-color: var(--color-main);
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  color: var(--color-text);
+  position: relative;
+  justify-content: center; /* Add this */
+  align-items: center; /* Add this */
+}
+
+.green-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  text-align: center; /* Add this for better text alignment */
+}
+
+.green-section.login {
+  border-radius: 0 120px 120px 0;
+}
+
+.green-section.register {
+  border-radius: 120px 0 0 120px;
+}
+
+/* Back to Home Button Styles */
+.back-to-home-btn {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  background: none;
+  color: var(--color-text);
+  border: none;
+  border-radius: 8px;
+  padding: 8px 12px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  backdrop-filter: blur(10px);
+  z-index: 10; /* Ensure it's above other content */
+}
+
+.back-to-home-btn:hover {
+  color: var(--color-background);
+}
+
+.back-to-home-btn-register {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  background: none;
+  color: var(--color-text);
+  border: none;
+  border-radius: 8px;
+  padding: 8px 12px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  backdrop-filter: blur(10px);
+  z-index: 10; /* Ensure it's above other content */
+
+}
+
+.back-to-home-btn-register:hover {
+  color: var(--color-background);
+}
+
+.back-to-home-btn span {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+/* Ensure text styling is consistent */
+.green-section h2 {
+  font-size: 28px;
+  font-family: Poppins, sans-serif;
+  font-weight: 600;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.green-section p {
+  margin-bottom: 20px;
+  text-align: center;
+  opacity: 0.9;
+}
+
+.form-fields {
+  width: 50%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-group {
+  margin-bottom: 20px;
+}
+
+input {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  font-size: 16px;
+}
+
+.submit-btn {
+  width: 100%;
+  padding: 10px 20px;
+  margin-top: 10px;
+  background-color: var(--color-main);
+  color: var(--color-button);
+  border: 1.5px solid transparent;
+  border-radius: 8px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  align-items: center;
+}
+
+.submit-btn:hover {
+  background-color: var(--color-background);
+  color: var(--color-text);
+  border: 1.5px solid var(--color-main);
+}
+
+.switch-btn {
+  padding: 10px 20px;
+  margin-top: 30px;
+  background-color: var(--color-button);
+  color: var(--color-background);
+  border: 1.5px solid transparent;
+  border-radius: 8px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  align-items: center;
+}
+
+.switch-btn:hover {
+  background-color: var(--color-background);
+  color: var(--color-button);
+  border: 1.5px solid var(--color-button);
+}
+
+.forgot-password {
+  font-size: 14px;
+  display: block;
+  text-align: center;
+  color: #666;
+  text-decoration: none;
+  margin: 10px 0;
+}
+
+.error {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #ff4444;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 8px;
+}
+
+/* Animation classes */
+.slide-left .login {
+  transform: translateX(-100%);
+}
+
+.register {
+  transform: translateX(100%);
+}
+
+.slide-left .register {
+  transform: translateX(0);
+}
+
+h1 {
+  margin-bottom: 30px;
+  color: var(--color-text);
+}
+
+h2 {
+  margin-bottom: 20px;
+  color: var(--color-text);
+}
+</style>
--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -221,11 +221,35 @@ const setTransactionType = (type) => {
 
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
 .dashboard-container {
   display: flex;
   background-color: var(--color-main);
   height: 100vh;
+  width: 100vw; /* Add this */
   overflow: hidden;
+  margin: 0; /* Add this */
+  padding: 0; /* Add this */
 }
 
 .side-menu {
