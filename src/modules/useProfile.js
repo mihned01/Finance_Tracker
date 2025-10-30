01c340a909e2cebd4b5c2b172a7934abe8d146ed
mihned01 <mihned01@easv365.dk>
2025-10-30 18:38:59 +0100

    got the logic back

--- src/modules/useProfile.js
+++ src/modules/useProfile.js
@@ -4,6 +4,7 @@ import { doc, deleteDoc, collection, getDocs, writeBatch } from 'firebase/firest
 import { db } from './firebase'
 import { useAuth } from './useAuth'
 import { useTransactions } from './useTransactions'
+import { useCurrency } from './useCurrency'
 
 export function useProfile() {
   const { currentUser } = useAuth()
@@ -44,6 +45,7 @@ export function useProfile() {
     return balance.value >= 0 ? '+2.5% this month' : '-1.2% this month'
   })
 
+  const { formatCurrency } = useCurrency()
 
   // Methods
   const editProfile = () => {
@@ -253,6 +255,7 @@ export function useProfile() {
     lastLoginDate,
     balanceChangeClass,
     balanceChangeText,
+    formatCurrency,
 
     // Methods
     editProfile,
